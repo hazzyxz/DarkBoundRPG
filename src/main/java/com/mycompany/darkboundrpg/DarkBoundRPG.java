9bd98f579b8438ee5f5c962b62e59c2b064179e6
hazzyxz <hazimborkhan@gmail.com>
2023-10-29 00:00:57 +0800

    test push

--- src/main/java/com/mycompany/darkboundrpg/DarkBoundRPG.java
+++ src/main/java/com/mycompany/darkboundrpg/DarkBoundRPG.java
@@ -14,7 +14,7 @@ public class DarkBoundRPG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("This is a test pushhtjtyjtyyjrtjtyj");
+        System.out.println("Hello World!");
     }
     
 }
