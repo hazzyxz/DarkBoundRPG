7d59e822c8514713e529d5473cd9189fc77d1e1b
MeTheGreat1474 <ahmadtaufiq1474@gmail.com>
2023-10-30 12:43:46 +0800

    test package

--- src/main/java/com/mycompany/darkboundrpg/DarkBoundRPG.java
+++ src/main/java/com/mycompany/darkboundrpg/DarkBoundRPG.java
@@ -3,7 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package com.mycompany.darkboundrpg;
-
+import makkauhijau.yeetprint;
 /**
  *
  * @author User
@@ -14,6 +14,9 @@ public class DarkBoundRPG {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        yeetprint obj = new yeetprint();
+
+        obj.printyeet();
         System.out.println("Hello World!");
     }
     
---
+++ src/main/java/makkauhijau/yeetprint.java
@@ -0,0 +1,7 @@
+package makkauhijau;
+
+public class yeetprint {
+    public void printyeet() {
+        System.out.println("YEET");
+    }
+}
